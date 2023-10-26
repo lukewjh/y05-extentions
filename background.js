7c2465a4f39c0bf1ab699e296b92af0bc47352ec
宋朝辉 <song951129@163.com>
2023-10-26 17:29:10 +0800

    初次整理项目结构

--- background.js
+++ background.js
@@ -5,11 +5,4 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         
     }
 });
-// 发送消息给本地应用程序
-// chrome.runtime.sendNativeMessage('com.example.nativeapp', { message: 'Hello, Native App!' }, function(response) {
-//     if (chrome.runtime.lastError) {
-//         console.error(chrome.runtime.lastError);
-//     } else {
-//         console.log('收到本地应用程序的响应：', response);
-//     }
-// });
+
